58d14fa159330f49f0331db33d3c8af2923d480e
tanghouly <kdeebest@gmail.com>
2023-11-28 17:13:32 +0400

    fixed broken grades code with comments

--- broken_grades.py
+++ broken_grades.py
@@ -21,36 +21,37 @@
 
 exam_one = int(input("Input exam grade one: "))
 
-exam_two = input("Input exam grade two: "))
+exam_two = int(input("Input exam grade two: "))#need to ad int for type of data which is integer
 
-exam_3 = str(input("Input exam grade three: "))
+exam_three = int(input("Input exam grade three: "))#added int again for input
 
-grades = [exam_one exam_two exam_three]
+grades = [exam_one, exam_two ,exam_three]#commas  added and defined properly logical mistake
 sum = 0
-for grade in grade:
+for grade in grades:
   sum = sum + grade
 
-avg = sum / len(grdes)
+avg = sum / len(grades)#not spelled properly
 
 if avg >= 90:
     letter_grade = "A"
-elif avg >= 80 and avg < 90
+elif avg >= 80 and avg < 90:#colon added
     letter_grade = "B"
 elif avg > 69 and avg < 80:
-    letter_grade = "C'
+    letter_grade = "C"#need double quotes for syntax
 elif avg <= 69 and avg >= 65:
     letter_grade = "D"
-elif:
+else:#change to else rather than elif
     letter_grade = "F"
 
 for grade in grades:
-    print("Exam: " + str(grade))
+    print("Exam: " + str(grades))#must be defined as grades to print as list
 
     print("Average: " + str(avg))
 
-    print("Grade: " + letter_grade)
+    print("Grade: " + str(letter_grade))
+    break#break added to exit loop
 
-if letter-grade is "F":
-    print "Student is failing."
+if letter_grade is "F":#must be the same variable with "_"
+    print ("Student is failing.")#brackets added to print
 else:
-    print "Student is passing."
+    print( "Student is passing.")
