1b7595333f1d71d4ae50e194ba9134ef30bebfa4
tanghouly <kdeebest@gmail.com>
2023-11-28 17:15:21 +0400

    fixed broken travel code with comments added

--- broken_travel.py
+++ broken_travel.py
@@ -6,11 +6,11 @@
 # if he is from the distant past (before 1900),
 # the present era (1900-2020) or from the far future (beyond 2020).
 
-year == int.input("Greetings! What is your year of origin? '))
-
-if year <= 1900
-    print ('Woah, that's the past!')
-elif year > 1900 && year < 2020:
+year =int(input("Greetings! What is your year of origin? "))#removed the double equals
+#added the same quotes for input
+if year <= 1900:
+    print ("Woah, that's the past!")#added double quotes for input
+elif year > 1900 and year < 2020:#changed %% to and for elif to work properly
     print ("That's totally the present!")
-elif:
+else:#elif changed to else for correct operation
     print ("Far out, that's the future!!")
