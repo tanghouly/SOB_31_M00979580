f6977a1f03d8bf715df51e3043643887ee1b89c3
tanghouly <kdeebest@gmail.com>
2023-11-28 17:16:16 +0400

    completed cowbulls game by adding missing code along with comments

--- cowbulls_incomplete.py
+++ cowbulls_incomplete.py
@@ -1,13 +1,20 @@
 import random
-
+## your code here
 def compare_numbers(number, user_guess):
-    ## your code here
-    return cowbull
+    cow=0
+    bull=0
+    for i in range(4):
+        if user_guess[i] ==number[i]:
+            bull+=1
+        elif user_guess[i] in number:
+                cow+=1
+    number=cow,bull
+    return number#changed this so number is returned instead of cowbull
 
 playing = True #gotta play the game
 number = str(random.randint(0,9999)) #random 4 digit number
 guesses = 0
-print number
+#print(number)#print number removed user is not supposed to know also used to test code works
 
 print("Let's play a game of Cowbull!") #explanation
 print("I will generate a number, and you have to guess the numbers one digit at a time.")
@@ -16,7 +23,7 @@ print("The game ends when you get 4 bulls!")
 print("Type exit at any prompt to exit.")
 
 while playing:
-    user_guess = raw_input("Give me your best guess!")
+    user_guess = input("Give me your best guess!")#replaced raw_input with input
     if user_guess == "exit":
         break
     cowbullcount = compare_numbers(number,user_guess)
